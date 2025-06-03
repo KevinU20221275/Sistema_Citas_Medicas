97902d981593b7f871a265ebe54a8fcad8b2f935
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-02 19:32:25 -0600

    add medico card

---
+++ src/components/medicos/MedicoCard.tsx
@@ -0,0 +1,68 @@
+// import types
+import type { IMedicoInfo } from "src/types/IMedicoInfo"
+import type { IHorarioInfo } from "src/types/IHorarioInfo";
+
+// import icons
+import { MedicoIcon } from "../icons/Medico"
+import { UpdateIcon } from "../icons/Update";
+import { DeleteIcon } from "../icons/Delete";
+
+// import store
+import { useHorariosStore } from "src/store/useHorariosStore"
+
+interface CardProps {
+    medico : IMedicoInfo;
+    agregarHorario: (id:string) => void;
+    actualizarHorario: (horario: IHorarioInfo) => void;
+}
+
+export function MedicoCard({medico, agregarHorario, actualizarHorario}: CardProps){
+    const {id, nombre, apellido, dui, sexo, telefono, direccion, especialidad} = medico
+    const horarios = useHorariosStore((state) => state.horarios)
+    const eliminarHorario = useHorariosStore((state) => state.eliminarHorario)
+
+    const horariosDelMedico = horarios.filter((h) => h.medicoId === id)
+
+    return (
+        <div className="flex flex-col justify-between bg-white rounded-md p-4 hover:shadow-md hover:shadow-zinc-400 transition-all">
+            <picture className="flex justify-center text-indigo-500">
+                <MedicoIcon className="w-20" />
+            </picture>
+            <div>
+                <p><span className="text-indigo-600">Medico:</span> {nombre + ' ' + apellido}</p>
+                <p><span className="text-indigo-600">Especialidad:</span> {especialidad}</p>
+                <p><span className="text-indigo-600">Telefono:</span> {telefono}</p>
+                <p><span className="text-indigo-600">Direccion:</span> {direccion}</p>
+            </div>
+            <div className="">
+                <span className="text-indigo-600">Horarios:</span>
+                <ul className="flex flex-col items-start justify-center list-disc pl-7">
+                    {
+                        horariosDelMedico.map((h) => (
+                            <li key={h.id}>{h.dia}: {h.horaInicio} - {h.horaFin}
+                                <button onClick={() => actualizarHorario(h)} className="bg-indigo-300 p-1 rounded-md cursor-pointer mx-1"><UpdateIcon className="w-4" /></button> 
+                                <button onClick={() => eliminarHorario(h.id)} className="bg-red-600 p-1 rounded-md text-white cursor-pointer"><DeleteIcon className="w-4"/></button>
+                            </li>
+                        ))
+                    }
+                    {horariosDelMedico.length === 0 && <p>No hay horarios</p>}
+                </ul>
+            </div>
+            <div className="grid grid-cols-2 gap-1.5 text-center py-2">
+                <button
+                onClick={() => agregarHorario(id)}
+                className="px-3 py-1 bg-indigo-400 rounded-md text-xs text-white hover:bg-indigo-500 transition-all cursor-pointer"
+                >Agregar Horario
+                </button>
+
+                <a href={`/medicos-form/${id}`}
+                className="px-3 py-1 bg-indigo-400 rounded-md text-xs text-white hover:bg-indigo-500 transition-all"
+                >Editar Medico</a>
+                
+                <button
+                className="px-3 py-1 bg-indigo-400 rounded-md text-xs text-white cursor-pointer hover:bg-indigo-500 transition-all"
+                >Eliminar Medico</button>
+            </div>
+        </div>
+    )
+}
