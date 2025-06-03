bad26ac0c587d167c002a44eb288254de7ff8946
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-02 19:26:11 -0600

    add cita, close, delete and update icons

---
+++ src/components/icons/CitaIcon.tsx
@@ -0,0 +1,23 @@
+export function CitaIcon({ className }: { className: string }) {
+  return (
+    <svg
+      fill="currentColor"
+      className={className}
+      viewBox="0 0 24 24"
+      data-name="Layer 1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <title></title>
+        <path d="M18,5V3a1,1,0,0,0-2,0V5H8V3A1,1,0,0,0,6,3V5H2V21H22V5Zm2,14H4V7H20ZM9,10H7v2H9Zm0,4H7v2H9Zm8-4H11v2h6Zm0,4H11v2h6Z"></path>
+      </g>
+    </svg>
+  );
+}
