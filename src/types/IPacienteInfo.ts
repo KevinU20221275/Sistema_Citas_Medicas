e5c3627685a94f121796237296b4b3a007a601c3
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-02 19:28:25 -0600

    add interface IPacienteInfo

---
+++ src/types/IPacienteInfo.ts
@@ -0,0 +1,12 @@
+export interface IPacienteInfo {
+  id: string;
+  nombre: string;
+  apellido: string;
+  dui: string;
+  edad: number;
+  telefono: string;
+  direccion: string;
+  sexo: string;
+  peso: number;
+  altura: number;
+}
