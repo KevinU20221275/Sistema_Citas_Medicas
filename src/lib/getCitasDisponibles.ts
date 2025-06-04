1aa9e800c0dfd44fddced5e5921b14501fe7cab8
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-03 20:16:37 -0600

    add getCitasDisponibles function

---
+++ src/lib/getCitasDisponibles.ts
@@ -0,0 +1,43 @@
+import type { Horario } from "src/models/Horario"
+import { useCitaStore } from "src/store/useCitasStore"
+
+export const getCitasDisponibles = (fecha: Date, horarios: Horario[], medicoId: string) =>{
+    const citas = useCitaStore.getState().citas
+    const posiblesCitas : string[] = []
+
+    if (horarios.length > 0){
+        horarios.forEach((h) => {
+            let [hi, hiMin] = h.horaInicio.split(":").map(Number)
+            let [hf, hfMin] = h.horaFin.split(":").map(Number)
+
+            let inicioEnMin = hi * 60 + hiMin
+            const finEnMin = hf * 60 + hfMin
+   
+            while (inicioEnMin + 20 <= finEnMin){
+                const horas = Math.floor(inicioEnMin / 60)
+                const minutos = inicioEnMin % 60
+
+                const cita = `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}`
+                posiblesCitas.push(cita)
+                
+                inicioEnMin += 20
+            }
+        })
+    }
+
+    const citasDelDia = citas.filter((c) => {
+        const citaFecha = new Date(c.fecha)
+        return (
+            citaFecha.getFullYear() === fecha.getFullYear() &&
+            citaFecha.getMonth() === fecha.getMonth() &&
+            citaFecha.getDay() === fecha.getDay() &&
+            c.medicoId == medicoId
+        )
+    })
+
+    const horasOcupadas = new Set(citasDelDia.map((c) => c.hora))
+
+    const disponibles = posiblesCitas.filter((hora) => !horasOcupadas.has(hora))
+   
+    return disponibles
+}
