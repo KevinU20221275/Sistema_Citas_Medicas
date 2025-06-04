0ca9420c78f6b9abe4a8b88d0b896bbc647bc094
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-03 20:18:09 -0600

    add actualizarEstadoModal component

---
+++ src/components/citas/ActualizarEstadoModal.tsx
@@ -0,0 +1,67 @@
+import { CloseIcon } from "../icons/CloseIcon";
+import { useState } from "react";
+import { useCitaStore } from "src/store/useCitasStore";
+import { EstadoConsulta } from "src/types/EstadosConsulta";
+
+
+
+export function ActualizarEstadoModal({id, estado, closeModal}:{ id:string, estado: EstadoConsulta, closeModal:(value:boolean) => void}){
+    const [estadoCita, setEstadoCita] = useState(estado)
+    const [isClosing, setIsClosing] = useState(false)
+    const actualizarEstado = useCitaStore((state) => state.actualizarEstado)
+
+    const handleSubmit = () => {
+        actualizarEstado(id, estadoCita)
+        closeModal(false)
+    }
+
+    return (
+        <div className="absolute z-50 flex justify-center items-center top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-black/40 w-full h-full">
+            <form onSubmit={(e) => {
+            e.preventDefault()
+            handleSubmit()
+            }} 
+            className={`flex flex-col relative gap-2 bg-white p-6 rounded-md w-80 ${isClosing ? 'fadeOut' : 'fadeIn'}`}
+            >
+                
+                <button 
+                onClick={(e) => {
+                    e.preventDefault();
+                    setIsClosing(true)
+                    setTimeout(() => {
+                        closeModal(false)
+                        setIsClosing(false)
+                    }, 400)
+                }}
+                className="absolute top-2 right-2 hover:bg-zinc-300 rounded-full p-1 transition-all cursor-pointer">
+                    <CloseIcon className="w-5"/>
+                </button>
+                
+                <fieldset>
+                    <label htmlFor="hora" className="block mb-1">Hora <span className="text-red-600">*</span></label>
+                    <select name="hora" id="hora" required
+                    className={`p-2 border-[1px] rounded-lg bg-white w-full border-zinc-300`}
+                    onChange={(e) => {
+                        const value = e.target.value
+                        
+                        const nuevoEstado = value === EstadoConsulta.Cancelada ? EstadoConsulta.Cancelada :
+                        value === EstadoConsulta.Completada ? EstadoConsulta.Completada :
+                        EstadoConsulta.Pendiente
+
+                        setEstadoCita(nuevoEstado)
+                    }}
+                    defaultValue={estado}
+                    >
+                        <option value="">Seleccione el estado de la Cita</option>
+                        <option value={EstadoConsulta.Completada}>{EstadoConsulta.Completada}</option>
+                        <option value={EstadoConsulta.Pendiente}>{EstadoConsulta.Pendiente}</option>
+                        <option value={EstadoConsulta.Cancelada}>{EstadoConsulta.Cancelada}</option>
+                    </select>
+                </fieldset>
+                <fieldset className="flex justify-center items-center pt-1">
+                    <button className="py-1.5 px-3 text-white bg-indigo-400 hover:bg-indigo-500 rounded-md cursor-pointer">Actualizar</button>
+                </fieldset>
+            </form>
+        </div>
+    )
+}
