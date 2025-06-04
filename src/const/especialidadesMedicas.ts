7c37fc3f6a96eb7a6c76021644a4458dc31f0ad8
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-03 20:11:37 -0600

    add const folder and especialidadesMedicas const

---
+++ src/const/especialidadesMedicas.ts
@@ -0,0 +1,15 @@
+export const ESPECIALIDADES_MEDICAS = [
+    {
+        "especialidad" : "Pediatría" 
+    }
+    ,
+    {
+        "especialidad" : "Medicina Interna"
+    },
+    {
+        "especialidad" : "Psiquiatría" 
+    },
+    {
+        "especialidad" : "Medicina General"
+    }    
+]
