d9a22bec568e31be19887b584b24befc33cf5cc6
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-03 20:13:06 -0600

    add pacientes dashboard

---
+++ src/components/pacientes/PacientesDashboard.tsx
@@ -0,0 +1,34 @@
+import { usePacienteStore } from "src/store/usePacientesStore"
+import { PacienteCard } from "./PacienteCard"
+import { useMemo, useState } from "react"
+
+export function PacientesDashboard(){
+    const [search, setSearch] = useState<string>('')
+
+    const pacientes = usePacienteStore((state) => state.pacientes)
+
+    const pacientesFiltrados = useMemo(() => {
+        if (search.length > 0){
+            return pacientes.filter((p) => 
+                p.nombre.toLocaleLowerCase().includes(search.toLocaleLowerCase()) || 
+                p.apellido.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+        } else {
+            return pacientes
+        }
+    }, [search])
+
+    return (
+        <section className="grid grid-cols-4 gap-2">
+            <div className="col-span-4">
+                <input type="text" placeholder="Buscar paciente por Nombre o Apellido" className="px-4 bg-white py-2 rounded-md w-96"
+                onChange={(e) => setSearch(e.target.value)}
+                defaultValue={search}
+                />
+            </div>
+            {
+                pacientesFiltrados.map((p) => <PacienteCard key={p.id} paciente={p} />)
+            }
+            {pacientesFiltrados.length === 0 && <p>Sin resultados</p>}
+        </section>
+    )
+}
