ec096666bb45493aec8fcdb576b13d7314e39946
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-04 20:50:31 -0600

    add commentsa and remove unused imports

--- src/components/medicos/MedicosDashboard.tsx
+++ src/components/medicos/MedicosDashboard.tsx
@@ -1,14 +1,15 @@
-import { useMedicoStore } from "src/store/useMedicoStore";
 import { useState } from "react";
 import type { IHorarioInfo } from "src/types/IHorarioInfo";
 import { MedicoCard } from "./MedicoCard";
 import { FormModalHorario } from "./FormModalHorario";
+import { useMedicosFilters } from "src/hooks/useMedicosFilter";
+import { MedicosFilterPanel } from "./MedicosFilterView";
 
 export function MedicosDashboard(){
-    const medicos = useMedicoStore((state) => state.medicos)
     const [showModal, setShowModal] = useState(false)
     const [horario, setHorario] = useState<IHorarioInfo | undefined>()
     const [medicoId,setMedicoId] = useState<string | undefined>(undefined)
+    const { medicosFiltrados, filter, changeFilter } = useMedicosFilters()
 
     const handleAgregarhorario = (id: string) => {
         setMedicoId(id)
@@ -20,11 +21,14 @@ export function MedicosDashboard(){
         setShowModal(true)
     }
 
+    console.log(medicosFiltrados)
+
     return (
         <section className="grid grid-cols-4 gap-2">
+            <MedicosFilterPanel className={'col-span-4 flex flex-col items-center text-center pb-4'} filter={filter} changeFilter={changeFilter} />
             {showModal && <FormModalHorario horario={horario} medicoId={medicoId} closeModal={setShowModal} resetHorarioData={setHorario} />}
             {
-                medicos.map((m) => <MedicoCard key={m.id} medico={m} agregarHorario={handleAgregarhorario} actualizarHorario={handleEditHorario} />)
+                medicosFiltrados.map((m) => <MedicoCard key={m.id} medico={m} agregarHorario={handleAgregarhorario} actualizarHorario={handleEditHorario} />)
             }
         </section>
     )
