59ba8dea930c5482845e18232fbdda09b510ede2
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-04 20:33:30 -0600

    implement useCurrentPath logic in Header and Aside components

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,13 +1,24 @@
+import { useCurrentPath } from "src/store/useCurrentPath";
 import { CitaIcon } from "./icons/CitaIcon";
 import { MedicoIcon } from "./icons/Medico";
 import { PacienteIcon } from "./icons/Paciente";
+import { useEffect, useState } from "react";
 
 export function AsideMenu(){
+    const currentPath = useCurrentPath()
+    const [isClient, setIsClient] = useState(false)
+    
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+    
+    if (!isClient) return null
+
     return (
         <ul>
             <li>
                 <a href="/medicos"
-                className="flex items-center gap-1.5 px-5 py-2 rounded-md hover:bg-indigo-400 hover:text-white transition-all"
+                className={`flex items-center gap-1.5 px-5 py-2 rounded-md ${currentPath === '/medicos' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'hover:bg-indigo-400'} hover:text-white transition-all`}
                 >
                     <MedicoIcon className={'w-5'}/>
                     Medicos
@@ -15,7 +26,7 @@ export function AsideMenu(){
             </li>
             <li>
                 <a href="/pacientes"
-                className="flex items-center gap-1.5 px-5 py-2 rounded-md hover:bg-indigo-400 hover:text-white  transition-all"
+                className={`flex items-center gap-1.5 px-5 py-2 rounded-md ${currentPath === '/pacientes' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'hover:bg-indigo-400'} hover:text-white transition-all`}
                 >
                     <PacienteIcon className={'w-5'}/>
                     Pacientes
@@ -23,7 +34,7 @@ export function AsideMenu(){
             </li>
             <li>
                 <a href="/citas"
-                className="flex items-center gap-1.5 px-5 py-2 rounded-md hover:bg-indigo-400 hover:text-white transition-all"
+                className={`flex items-center gap-1.5 px-5 py-2 rounded-md ${currentPath === '/citas' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'hover:bg-indigo-400'} hover:text-white transition-all`}
                 >
                     <CitaIcon className={'w-5'} />
                     Citas
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react"
+import { useCurrentPath } from "src/store/useCurrentPath"
+
 export function HeaderMenu(){
+    const currentPath = useCurrentPath()
+    const [isClient, setIsClient] = useState(false)
+
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+
+    if (!isClient) return null
+    
     return (
         <>
             <h2 className="text-indigo-600 text-xl">Sistema Citas Medicas</h2>
@@ -6,22 +18,22 @@ export function HeaderMenu(){
                 <ul className="flex gap-2">
                     <li>
                         <a href="/"
-                        className="flex p-2 px-3 rounded-full hover:bg-indigo-400 hover:text-white text-sm transition-all"
+                        className={`flex p-2 px-3 rounded-full ${currentPath === '/' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'hover:bg-indigo-400'} hover:text-white text-sm transition-all`}
                         >Home</a>
                     </li>
                     <li>
                         <a href={`/pacientes-form/${'nuevoPaciente'}`}
-                        className="flex p-2 px-3 rounded-full hover:bg-indigo-400 hover:text-white text-sm transition-all"
+                        className={`flex p-2 px-3 rounded-full ${currentPath === '/pacientes-form/nuevoPaciente' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'hover:bg-indigo-400'} hover:text-white text-sm transition-all`}
                         >Agregar Paciente</a>
                     </li>
                     <li>
                         <a href={`/medicos-form/${'nuevoMedico'}`}
-                        className="flex p-2 px-3 rounded-full hover:bg-indigo-400 hover:text-white text-sm transition-all"
+                        className={`flex p-2 px-3 rounded-full ${currentPath === '/medicos-form/nuevoMedico' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'hover:bg-indigo-400'} hover:text-white text-sm transition-all`}
                         >Agregar Medico</a>
                     </li>
                     <li>
                         <a href={`/agendarCita/${'nuevaCita'}`}
-                        className="flex p-2 px-3 rounded-full hover:bg-indigo-400 hover:text-white text-sm transition-all"
+                        className={`flex p-2 px-3 rounded-full ${currentPath === '/agendarCita/nuevaCita' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'hover:bg-indigo-400'} hover:text-white text-sm transition-all`}
                         >Agendar Cita</a>
                     </li>
                 </ul>
