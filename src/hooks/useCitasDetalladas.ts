febdf9973730fd11b3d452392406031d4703e208
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-04 20:36:25 -0600

    update useCitasDetalladas hook to return citas order by date

--- src/hooks/useCitasDetalladas.ts
+++ src/hooks/useCitasDetalladas.ts
@@ -1,3 +1,4 @@
+import type { CitaDetallada } from "src/types/definitions"
 import { useCitaStore } from "src/store/useCitasStore"
 import { useMedicoStore } from "src/store/useMedicoStore"
 import { usePacienteStore } from "src/store/usePacientesStore"
@@ -6,8 +7,15 @@ export function useCitasDetalladas(){
     const citas = useCitaStore((state) => state.citas)
     const pacientes = usePacienteStore((state) => state.pacientes)
     const medicos = useMedicoStore((state) => state.medicos)
+
+    const citasProximas = [...citas]
+    .filter(c => c.obtenerFecha() > new Date())
+    .toSorted((a,b) => a.obtenerFecha().getTime() - b.obtenerFecha().getTime())
+
+    const todasLasCitasOrdenadasPorFecha = [...citas]
+    .toSorted((a,b) => a.obtenerFecha().getTime() - b.obtenerFecha().getTime())
     
-    const citasDetalladas = citas.map((c) => {
+    const todasLasCitasDetalladas : CitaDetallada[] = todasLasCitasOrdenadasPorFecha.map((c) => {
         const paciente = pacientes.find(p => p.id === c.pacienteId)
         const medico = medicos.find(m => m.id === c.medicoId)
     
@@ -18,5 +26,16 @@ export function useCitasDetalladas(){
         }
     })
 
-    return { citasDetalladas }
+    const citasDetalladasProximas : CitaDetallada[] = citasProximas.map((c) => {
+        const paciente = pacientes.find(p => p.id === c.pacienteId)
+        const medico = medicos.find(m => m.id === c.medicoId)
+    
+        return {
+            ...c,
+            paciente: paciente ? `${paciente.nombre + ' ' + paciente.apellido}` : 'Desconocido',
+            medico: medico ? `${medico.nombre + ' ' + medico.apellido}` : 'Desconocido'
+        }
+    })
+
+    return { todasLasCitasDetalladas, citasDetalladasProximas }
 }
