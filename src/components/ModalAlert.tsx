a3c14c0b2ccd1f52b0b98a7ae22693a4d1d7fa1c
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-06 11:42:15 -0600

    update modal

--- src/components/ModalAlert.tsx
+++ src/components/ModalAlert.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { CloseIcon } from "./icons/CloseIcon";
+import { navigate } from "astro:transitions/client";
 
-export function ModalAlert({mensaje, redirect, closeModal, isEditing}: {mensaje: string, redirect:string, closeModal:(value: boolean) => void, isEditing?:string}){
+export function ModalAlert({mensaje, redirect, reload, closeModal, isEditing}: {mensaje: string, redirect:string, reload?:string, closeModal:(value: boolean) => void, isEditing?:string}){
     const [isClosing, setIsClosing] = useState(false)
     return (
         <div className="absolute flex z-20 justify-center items-center top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-black/40 w-full h-full">
@@ -22,13 +23,14 @@ export function ModalAlert({mensaje, redirect, closeModal, isEditing}: {mensaje:
                 <p className="text-center text-2xl text-green-600 py-4">{mensaje}</p>
                 <div className="flex justify-center items-center gap-2 text-sm text-white">
                     <a href={redirect} className={'px-3 py-1 rounded-md bg-indigo-500 dark:bg-indigo-700 hover:bg-indigo-600'}>Ir al Dashboard</a>
-                    {!isEditing && <button 
+                    {(!isEditing && reload) && <button 
                     onClick={(e) => {
                         e.preventDefault();
                         setIsClosing(true)
                         setTimeout(() => {
                                 closeModal(false)
                                 setIsClosing(false)
+                                navigate(reload)
                         }, 400)
                     }}
                     className="px-3 py-1 rounded-md bg-indigo-500 dark:bg-indigo-700 hover:bg-indigo-600 cursor-pointer">Seguir agregando registros</button>}
