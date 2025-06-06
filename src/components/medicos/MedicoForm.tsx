1407fd0139100ea55a75628572206d9bdd668673
KevinU20221275 <U20221275@univo.edu.sv>
2025-06-05 20:38:42 -0600

    add validations

--- src/components/medicos/MedicoForm.tsx
+++ src/components/medicos/MedicoForm.tsx
@@ -1,5 +1,5 @@
 // import hooks
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMedicoStore } from "src/store/useMedicoStore";
 // import types
 import type { IMedicoInfo } from "src/types/IMedicoInfo";
@@ -8,23 +8,23 @@ import { Generos } from "src/types/Genero";
 import { MedicoIcon } from "../icons/Medico";
 // import const
 import { ESPECIALIDADES_MEDICAS } from "src/const/especialidadesMedicas";
-
+import { INITIAL_STATE_MEDICO_FORM } from "@src/const/initialsStates";
+// import components
+import { ModalAlert } from "../ModalAlert";
+import { validateMedicoForm } from "@src/lib/validarMedicoForm";
 
 export function MedicoForm({id} : {id? : string}){
     const paramId = id === "nuevoMedico" ? undefined : id
-    const getMedicoById = useMedicoStore((state) => state.getMedicoById)
-    const actualizarMedico = useMedicoStore((state) => state.actualizarMedico)
-    const agregarMedico = useMedicoStore((state) => state.agregarMedico)
-
+    const {getMedicoById, actualizarMedico, agregarMedico} = useMedicoStore((state) => state)
     const [medicoData, setMedicoData] = useState<IMedicoInfo>({
-        id : paramId ?? crypto.randomUUID(),
-        nombre :  '',
-        apellido : '',
-        dui : '',
-        telefono : '',
-        direccion : '',
-        sexo: '',
-        especialidad : '',
+        ...INITIAL_STATE_MEDICO_FORM,
+        id : crypto.randomUUID()
+    })
+    const [showModal, setShowModal] = useState(false)
+    const formRef = useRef<HTMLFormElement | null>(null)
+    const [errors,setErrors] = useState({
+        duiError : '',
+        telefonoError : '',
     })
 
     useEffect(() => {
@@ -37,24 +37,45 @@ export function MedicoForm({id} : {id? : string}){
     }, [paramId, getMedicoById])
 
     const handleSubmit = () => {
+        const {isValid, error} = validateMedicoForm(medicoData)
+        if (!isValid){
+            setErrors(error)
+            return
+        }
+
         if (paramId){
             actualizarMedico(medicoData)
         } else {
             agregarMedico(medicoData)
         }
+        
+        setShowModal(true)
+        formRef.current?.reset()
+        setMedicoData({
+            ...INITIAL_STATE_MEDICO_FORM,
+            id : crypto.randomUUID()
+        })
     }
 
     return (
         <section className="p-6">
             <h3 className="text-2xl text-center text-indigo-600 font-medium">Ingrese la informacion del medico</h3>
+            {showModal && <ModalAlert redirect="/medicos" mensaje={`Medico ${paramId ? 'actualizado' : 'agregado'} con exito`} closeModal={setShowModal} isEditing={paramId} />}
             <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-6 gap-3">
-                <form action=""
-                onSubmit={handleSubmit}
+                <form ref={formRef} action=""
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    setErrors({
+                        duiError : '',
+                        telefonoError: ''
+                    })
+                    handleSubmit()
+                }}
                 className="grid grid-cols-2 gap-5 w-full md:col-span-2"
                 >
                     <fieldset>
                         <label htmlFor="nombre" className="block mb-1">Nombre</label>
-                        <input type="text" id="nombre" placeholder="Juan Simon"
+                        <input type="text" id="nombre" placeholder="Juan Simon" required
                         className="p-2 border-[1px] border-zinc-300 rounded-lg bg-white w-full"
                         onChange={(e) => setMedicoData({
                             ...medicoData,
@@ -65,7 +86,7 @@ export function MedicoForm({id} : {id? : string}){
                     </fieldset>
                     <fieldset>
                         <label htmlFor="apellido" className="block mb-1">Apellido</label>
-                        <input type="text" id="apellido" placeholder="Perez Gonzales"
+                        <input type="text" id="apellido" placeholder="Perez Gonzales" required
                         className="p-2 border-[1px] border-zinc-300 rounded-lg bg-white w-full"
                         onChange={(e) => setMedicoData({
                             ...medicoData,
@@ -76,7 +97,7 @@ export function MedicoForm({id} : {id? : string}){
                     </fieldset>
                     <fieldset>
                         <label htmlFor="dui" className="block mb-1">Dui</label>
-                        <input type="text" id="dui" placeholder="09090909-8"
+                        <input type="text" id="dui" placeholder="09090909-8" required maxLength={10} minLength={10}
                         className="p-2 border-[1px] border-zinc-300 rounded-lg bg-white w-full"
                         onChange={(e) => setMedicoData({
                             ...medicoData,
@@ -84,10 +105,11 @@ export function MedicoForm({id} : {id? : string}){
                         })}
                         defaultValue={medicoData.dui}
                         />
+                        <span className="text-xs text-red-600">{errors.duiError}</span>
                     </fieldset>
                     <fieldset>
                         <label htmlFor="telefono" className="block mb-1">Telefono</label>
-                        <input type="text" id="telefono" placeholder="7875-9090"
+                        <input type="text" id="telefono" placeholder="7875-9090" required maxLength={9} minLength={9}
                         className="p-2 border-[1px] border-zinc-300 rounded-lg bg-white w-full" 
                         onChange={(e) => setMedicoData({
                             ...medicoData,
@@ -95,10 +117,11 @@ export function MedicoForm({id} : {id? : string}){
                         })}
                         defaultValue={medicoData.telefono}
                         />
+                        <span className="text-xs text-red-600">{errors.telefonoError}</span>
                     </fieldset>
                     <fieldset>
                         <label htmlFor="genero" className="block mb-1">Genero</label>
-                        <select name="genero" id="genero"
+                        <select name="genero" id="genero" required
                         className="p-2 border-[1px] border-zinc-300 rounded-lg bg-white w-full"
                         onChange={(e) => setMedicoData({
                             ...medicoData,
@@ -107,7 +130,7 @@ export function MedicoForm({id} : {id? : string}){
                         value={medicoData.sexo}
                         
                         >
-                            <option value="">Seleccione el Generos</option>
+                            <option value="">Seleccione el Genero</option>
                             <option value={Generos.Masculino}>{Generos.Masculino}</option>
                             <option value={Generos.Femenino}>{Generos.Femenino}</option>
                             <option value={Generos.NoBinario}>{Generos.NoBinario}</option>
@@ -117,7 +140,7 @@ export function MedicoForm({id} : {id? : string}){
                     </fieldset>
                     <fieldset>
                         <label htmlFor="direccion" className="block mb-1">Direccion</label>
-                        <input type="text" id="direccion" placeholder="Barrio el Centro" 
+                        <input type="text" id="direccion" placeholder="Barrio el Centro" required
                         className="p-2 border-[1px] border-zinc-300 rounded-lg bg-white w-full"
                         onChange={(e) => setMedicoData({
                             ...medicoData,
@@ -128,7 +151,7 @@ export function MedicoForm({id} : {id? : string}){
                     </fieldset>
                     <fieldset>
                         <label htmlFor="genero" className="block mb-1">Especialidad</label>
-                        <select name="genero" id="genero"
+                        <select name="genero" id="genero" required
                         className="p-2 border-[1px] border-zinc-300 rounded-lg bg-white w-full"
                         onChange={(e) => setMedicoData({
                             ...medicoData,
